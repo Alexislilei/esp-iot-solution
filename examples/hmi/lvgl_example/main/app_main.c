5aca5621ac74c286f9f28c60cf8cc7d2c670ec75
Alexislilei <32153824+Alexislilei@users.noreply.github.com>
2021-09-08 21:18:58 +1000

    add IR function to main
    merge IR example from ESP examples to esp-iot-solution example

--- examples/hmi/lvgl_example/main/app_main.c
+++ examples/hmi/lvgl_example/main/app_main.c
@@ -27,6 +27,8 @@
 #include "lvgl_gui.h"
 #include "esp_log.h"
 
+#include "driver/rmt.h"
+
 #include "lv_examples/src/lv_demo_printer/lv_demo_printer.h"
 #include "lv_examples/src/lv_demo_widgets/lv_demo_widgets.h"
 #include "lv_examples/src/lv_ex_get_started/lv_ex_get_started.h"
@@ -35,7 +37,68 @@
 #include "lv_examples/src/lv_demo_stress/lv_demo_stress.h"
 #include "lv_examples/src/lv_ex_style/lv_ex_style.h"
 
+//#include "ir_demo.h"
+
 static const char *TAG = "example_lvgl";
+
+//============================================
+
+#include "ir_tools.h"
+
+
+
+//static rmt_channel_t example_tx_channel = RMT_CHANNEL_0;
+static rmt_channel_t example_rx_channel = RMT_CHANNEL_2;
+
+/**
+ * @brief RMT Receive Task
+ *
+ */
+static void example_ir_rx_task(void *arg)
+{
+    uint32_t addr = 0;
+    uint32_t cmd = 0;
+    size_t length = 0;
+    bool repeat = false;
+    RingbufHandle_t rb = NULL;
+    rmt_item32_t *items = NULL;
+
+    rmt_config_t rmt_rx_config = RMT_DEFAULT_CONFIG_RX(CONFIG_EXAMPLE_RMT_RX_GPIO, example_rx_channel);
+    rmt_config(&rmt_rx_config);
+    rmt_driver_install(example_rx_channel, 1000, 0);
+    ir_parser_config_t ir_parser_config = IR_PARSER_DEFAULT_CONFIG((ir_dev_t)example_rx_channel);
+    ir_parser_config.flags |= IR_TOOLS_FLAGS_PROTO_EXT; // Using extended IR protocols (both NEC and RC5 have extended version)
+    ir_parser_t *ir_parser = NULL;
+#if CONFIG_EXAMPLE_IR_PROTOCOL_NEC
+    ir_parser = ir_parser_rmt_new_nec(&ir_parser_config);
+#elif CONFIG_EXAMPLE_IR_PROTOCOL_RC5
+    ir_parser = ir_parser_rmt_new_rc5(&ir_parser_config);
+#endif
+
+    //get RMT RX ringbuffer
+    rmt_get_ringbuf_handle(example_rx_channel, &rb);
+    assert(rb != NULL);
+    // Start receive
+    rmt_rx_start(example_rx_channel, true);
+    while (1) {
+        items = (rmt_item32_t *) xRingbufferReceive(rb, &length, portMAX_DELAY);
+        if (items) {
+            length /= 4; // one RMT = 4 Bytes
+            if (ir_parser->input(ir_parser, items, length) == ESP_OK) {
+                if (ir_parser->get_scan_code(ir_parser, &addr, &cmd, &repeat) == ESP_OK) {
+                    ESP_LOGI(TAG, "Scan Code %s --- addr: 0x%04x cmd: 0x%04x", repeat ? "(repeat)" : "", addr, cmd);
+                }
+            }
+            //after parsing the data, return spaces to ringbuffer.
+            vRingbufferReturnItem(rb, (void *) items);
+        }
+    }
+    ir_parser->del(ir_parser);
+    rmt_driver_uninstall(example_rx_channel);
+    vTaskDelete(NULL);
+}
+
+//============================================
 
 void app_main()
 {
@@ -89,6 +152,10 @@ void app_main()
     /* Initialize LittlevGL GUI */
     lvgl_init(&lcd_drv, &touch_drv);
 
+    //=====================================
+    xTaskCreate(example_ir_rx_task, "ir_rx_task", 2048, NULL, 10, NULL); 
+    //=====================================
+
 #ifdef CONFIG_LV_DEMO_BENCHMARK
     lv_demo_benchmark();
 #elif defined CONFIG_LV_DEMO_PRINTER
@@ -106,3 +173,4 @@ void app_main()
     ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
     ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
 }
+
